8606e0a7e1e69107eab0e8ec539c83616515380f
Alexander Green <alexander.green@logius.nl>
2023-09-06 14:11:28 +0200

    regex

--- scripts/muffet-json.py
+++ scripts/muffet-json.py
@@ -9,7 +9,7 @@ with open('muffet.json') as file:
     data = json.load(file)
     data = sorted(data, key=lambda k: k['url'])
     for page in data:
-        if re.search("\.nl\/publicatie\/[^\/]", page['url']):
+        if re.search("publicatie\/[^\/]+\/[^\/]+\/$", page['url']):
             content += '\n### ' + page['url'] + '\n'
             page['links'] = sorted(page['links'], key=lambda k: k['url'])
             for link in page['links']:                
