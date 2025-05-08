1c58bcbf20af1d7b9237f9e5895d5655cbbd77e1
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-05-08 11:35:31 +0200

    Update compute-published-url.js

--- scripts/compute-published-url.js
+++ scripts/compute-published-url.js
@@ -1,6 +1,4 @@
 require("./js/config.js");
 const {pubDomain, shortName} = globalThis.respecConfig;
 
-// console.log(`${pubDomain}/${shortName}`);
-// TODO: remove voordat we mergen
-console.log(`api/adr`);
+console.log(`${pubDomain}/${shortName}`);
