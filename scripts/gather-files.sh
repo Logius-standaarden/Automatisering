7c877a1e0152271b1e43a2441b2253cc4b49a2b1
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-09-26 08:43:00 +0200

    Maak het mogelijk om meerdere documenten te bouwen (#63)
    Hiermee kan een standaard meerdere `index*.html` bestanden
    hebben en die worden allemaal apart gebouwd.

--- scripts/gather-files.sh
+++ scripts/gather-files.sh
@@ -1,7 +1,6 @@
 rm -rf ~/content
 mkdir ~/content
 cp ~/static/* ~/content/
-mv ~/content/snapshot.html ~/content/index.html
 if [ -d ./media ]; then
     mkdir -p ~/content/media/
     cp ./media/* ~/content/media/ 2>/dev/null || true
--- scripts/pdf.js
+++ scripts/pdf.js
@@ -1,11 +1,12 @@
 const puppeteer = require('puppeteer');
+const FILE_NAME = process.argv[2];
 
 (async () => {
     // The following is based on code from
     // https://www.bannerbear.com/blog/how-to-convert-html-into-pdf-with-node-js-and-puppeteer/
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const website_url = 'http://localhost:8080/snapshot.html';
+    const website_url = `http://localhost:8080/${FILE_NAME}`;
     await page.goto(website_url, { waitUntil: 'networkidle0' });
 
     const pdfName = await page.evaluate(() => {
@@ -14,9 +15,9 @@ const puppeteer = require('puppeteer');
       return `${initialUserConfig.pubDomain}-${initialUserConfig.shortName}-${initialUserConfig.publishVersion}.pdf`;
     });
     if (typeof pdfName !== 'string') {
-      throw new Error(`Couldn't obtain the name of the document. Got ${pdfName} instead`);
+      throw new Error(`Couldn't obtain the name in ${FILE_NAME}. Got ${pdfName} instead`);
     }
-    console.log(`Printing PDF with path ${pdfName}`);
+    console.log(`Printing PDF for ${FILE_NAME} with path ${pdfName}`);
 
     await page.emulateMediaType('print');
     await page.addStyleTag({ content: '.sidelabel {position: absolute}' })
