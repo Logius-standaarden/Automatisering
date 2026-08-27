1995732432dc83483d51e143abab384f1ea81c71
Tim van der Lippe <Tim.Lippe@logius.nl>
2026-08-27 14:57:23 +0200

    Genereer diff met previous URL

--- scripts/compute-published-url.mjs
+++ scripts/compute-published-url.mjs
@@ -6,9 +6,18 @@ try {
 
   const pubDomain = configText.match(/pubDomain['"]?:\s*['"](?<domain>[^'"]+)/).groups.domain;
   const shortName = configText.match(/shortName['"]?:\s*['"](?<shortname>[^'"]+)/).groups.shortname;
-  const publishVersion = configText.match(/publishVersion['"]?:\s*['"](?<publishversion>[^'"]+)/).groups.publishversion;
 
-  console.log(`${pubDomain}/${shortName}/${publishVersion}`);
+  if (process.argv.includes("--previous-url")) {
+    const previousPublishVersion = configText.match(/previousPublishVersion['"]?:\s*['"](?<previouspublishversion>[^'"]+)/)?.groups.previouspublishversion;
+
+    if (previousPublishVersion) {
+      console.log(`https://gitdocumentatie.logius.nl/publicatie/${pubDomain}/${shortName}/${previousPublishVersion}`);
+    }
+  } else {
+    const publishVersion = configText.match(/publishVersion['"]?:\s*['"](?<publishversion>[^'"]+)/).groups.publishversion;
+
+    console.log(`https://gitdocumentatie.logius.nl/publicatie/${pubDomain}/${shortName}/${publishVersion}`);
+  }
 } catch (err) {
   console.error(err);
   exit(1);
